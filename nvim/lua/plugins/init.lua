471478f8995c4a3b4e43f5385a1b55e5eca70fc7
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-15 15:40:52 -0300

    moved to native nvim package manager, removed lazy nvim

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,51 @@
+-- native nvim package manager
+
+vim.pack.add({
+
+	{
+		src = "https://github.com/ibhagwan/fzf-lua",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/neovim/nvim-lspconfig",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/rose-pine/neovim",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/stevearc/conform.nvim",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/echasnovski/mini.nvim",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/folke/which-key.nvim",
+		version = vim.version.range("*"),
+	},
+	{
+		src = "https://github.com/lewis6991/gitsigns.nvim",
+		version = vim.version.range("*"),
+	},
+})
+
+-- external config
+require("plugins.fzf")
+require("plugins.lsp")
+require("plugins.theme")
+require("plugins.treesitter")
+-- TODO: lazy load the plugin after saving a file
+require("plugins.formatter")
+
+-- no config needed
+require("mini.pairs").setup()
+require("mini.icons").setup()
+require("mini.surround").setup()
+require("gitsigns").setup()
