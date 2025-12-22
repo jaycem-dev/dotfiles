26f1d9a9d7c55668e6dc3f2c8130ffe7f194f3de
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-22 12:24:35 -0300

    change uwsm tty session display to default

--- fish/conf.d/hyprland.fish
+++ fish/conf.d/hyprland.fish
@@ -2,7 +2,7 @@
 if status is-login
     if test -z "$DISPLAY" -a (tty) = /dev/tty1
         if uwsm check may-start
-            exec uwsm start hyprland.desktop
+            uwsm start default
         end
     end
 end
