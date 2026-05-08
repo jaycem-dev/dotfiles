3d23b40bccf373ec866bd5df3feef597528785e9
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-05-08 00:24:52 -0400

    neovim toggleterm
    a

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@ vim.opt.winborder = "rounded"
 vim.opt.pumborder = "rounded"
 vim.opt.termguicolors = true -- Forces true color support in terminals
 vim.opt.cursorline = true
+vim.opt.laststatus = 3 -- Global statusline
 -- vim.opt.winblend = 10
 
 -- ui v2 experimental
