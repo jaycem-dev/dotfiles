087a03131097c047ac6eddca239225e2517ca53f
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-05-08 17:37:56 -0400

    nvim neogit added

---
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+	{
+		src = "https://github.com/NeogitOrg/neogit",
+	},
+	{
+		src = "https://github.com/sindrets/diffview.nvim",
+	},
+})
+
+vim.keymap.set("n", "<leader>g", "<cmd>Neogit<cr>", { desc = "Open Neogit UI" })
