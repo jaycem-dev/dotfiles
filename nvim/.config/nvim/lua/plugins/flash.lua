5f6f177d9206ed566dcf01dcfca13eb75a699640
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-05-10 16:52:11 -0400

    nvim fff plugin

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,8 +1,5 @@
 vim.pack.add({
-	{
-		src = "https://github.com/folke/flash.nvim",
-		version = vim.version.range("*"),
-	},
+	{ src = "https://github.com/folke/flash.nvim", version = vim.version.range("*") },
 })
 
 -- flash
