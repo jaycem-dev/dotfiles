bd769d6d36d035f707641303f10cd42e656f74af
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-05-11 01:01:26 -0400

    nvim plugin styling

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -4,17 +4,17 @@ vim.pack.add({
 
 vim.g.fff = {
 	lazy_sync = true,
-	debug = { enabled = true, show_scores = true },
+	-- debug = { enabled = true, show_scores = true },
 }
 
-vim.keymap.set("n", "<leader>ff", function()
+vim.keymap.set("n", "<leader>f", function()
 	require("fff").find_files()
 end, { desc = "Find files" })
 
-vim.keymap.set("n", "<leader>fg", function()
+vim.keymap.set("n", "<leader>/", function()
 	require("fff").live_grep()
 end, { desc = "Find files" })
 
-vim.keymap.set("n", "<leader>fw", function()
+vim.keymap.set("n", "<leader>w", function()
 	require("fff").live_grep({ query = vim.fn.expand("<cword>") })
 end, { desc = "Find current word" })
--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -4,6 +4,9 @@ vim.pack.add({
 
 local actions = require("fzf-lua.actions")
 require("fzf-lua").setup({
+	winopts = {
+		backdrop = 100,
+	},
 	keymap = {
 		fzf = {
 			["ctrl-a"] = "toggle-all",
