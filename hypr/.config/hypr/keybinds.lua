ead5157bb3092ae93881032fee98815102b3b9f8
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-05-20 18:18:07 -0400

    hyprland spawn or focus utils for apps,webapps,tuis

--- hypr/.config/hypr/keybinds.lua
+++ hypr/.config/hypr/keybinds.lua
@@ -1,24 +1,43 @@
 local mod = "SUPER" -- Sets "Windows" key as main modifier
--- local brightness = "~/.local/bin/brightness"
--- local volume = "~/.local/bin/volume"
+local brightness = "~/.local/bin/brightness"
+local volume = "~/.local/bin/volume"
 
+-- apps
 local terminal = { cmd = "foot" }
-local fileManager = { cmd = "kitty -1 --app-id yazi yazi", class = "yazi" }
 local menu = "fuzzel"
 local browser = { cmd = "brave", class = "brave-browser" }
 
--- launch or focus by class
+-- webapps, use only url
+local whatsapp = "web.whatsapp.com"
+local protonmail = "mail.proton.me"
+
+-- tui, class is optional
+local yazi = { cmd = "yazi" }
+local nvim = { cmd = "nvim -c 'lua _G.fzf_projects()'", class = "nvim-projects" }
+
+-- spawn or focus utils
 local function spawn_or_focus(app)
-	local class = app.class or app.cmd -- if class is not defined, use cmd
-	return function()
-		for _, w in ipairs(hl.get_windows()) do
-			if w.class == class then
-				hl.dispatch(hl.dsp.focus({ window = w }))
-				return
-			end
-		end
-		hl.dispatch(hl.dsp.exec_cmd(app.cmd))
-	end
+    return function()
+        local w = hl.get_window("class:" .. (app.class or app.cmd))
+        if w then
+            hl.dispatch(hl.dsp.focus({ window = w }))
+        else
+            hl.dispatch(hl.dsp.exec_cmd(app.cmd))
+        end
+    end
+end
+
+local function spawn_or_focus_webapp(url)
+    return spawn_or_focus({
+        cmd = "brave --app=https://" .. url,
+        class = "brave-" .. url .. "__-Default",
+    })
+end
+
+local function spawn_or_focus_tui(app)
+    local class = app.class or app.cmd
+    local cmd = "foot --app-id " .. class .. " " .. app.cmd
+    return spawn_or_focus({ cmd = cmd, class = app.class or app.cmd })
 end
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
@@ -27,11 +46,14 @@ hl.bind(mod .. " + SHIFT + F", hl.dsp.window.fullscreen())
 hl.bind(mod .. " + T", spawn_or_focus(terminal))
 hl.bind(mod .. " + Q", hl.dsp.window.close())
 hl.bind(
-	mod .. " + SHIFT + Q",
-	hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
+    mod .. " + SHIFT + Q",
+    hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
 )
-hl.bind(mod .. " + E", spawn_or_focus(fileManager))
+hl.bind(mod .. " + E", spawn_or_focus_tui(yazi))
+hl.bind(mod .. " + N", spawn_or_focus_tui(nvim))
 hl.bind(mod .. " + B", spawn_or_focus(browser))
+hl.bind(mod .. " + W", spawn_or_focus_webapp(whatsapp))
+hl.bind(mod .. " + M", spawn_or_focus_webapp(protonmail))
 hl.bind(mod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mod .. " + G", hl.dsp.window.pin())
 hl.bind(mod .. " + SPACE", hl.dsp.exec_cmd(menu))
@@ -52,14 +74,19 @@ hl.bind(mod .. " + SHIFT + down", hl.dsp.window.move({ direction = "down" }))
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
-	local key = i % 10 -- 10 maps to key 0
-	hl.bind(mod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-	hl.bind(mod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+    hl.bind(mod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
 -- Example special workspace (scratchpad)
 hl.bind(mod .. " + S", hl.dsp.workspace.toggle_special("magic"))
-hl.bind(mod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
+hl.bind(
+    mod .. " + SHIFT + S",
+    hl.dsp.exec_cmd(
+        "hyprctl dispatch tagwindow +scratchpad activewindow && hyprctl dispatch movetoworkspace special:magic"
+    )
+)
 
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(mod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
@@ -70,20 +97,20 @@ hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Laptop multimedia keys for volume and LCD brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("~/.local/bin/volume up"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("~/.local/bin/volume down"), { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(volume .. " up"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(volume .. " down"), { locked = true, repeating = true })
 hl.bind(
-	"XF86AudioMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
-	{ locked = true, repeating = true }
+    "XF86AudioMute",
+    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+    { locked = true, repeating = true }
 )
 hl.bind(
-	"XF86AudioMicMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
-	{ locked = true, repeating = true }
+    "XF86AudioMicMute",
+    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+    { locked = true, repeating = true }
 )
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("~/.local/bin/brightness up"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("~/.local/bin/brightness down"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(brightness .. " up"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(brightness .. " down"), { locked = true, repeating = true })
 
 -- Requires playerctl
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
