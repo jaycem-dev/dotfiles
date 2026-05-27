41fc5f52e56b4850c6d0e587f244c7d73ffce0e2
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-05-27 01:06:29 -0400

    hypr conf

--- hypr/.config/hypr/rules.lua
+++ hypr/.config/hypr/rules.lua
@@ -50,9 +50,10 @@ hl.window_rule({
 hl.layer_rule({
     match = { namespace = "waybar" },
     blur = true,
-    ignore_alpha = 0.5,
+    ignore_alpha = 0,
     xray = true,
 })
+
 hl.layer_rule({
     match = { namespace = "launcher" },
     blur = true,
@@ -95,3 +96,20 @@ hl.window_rule({
     move = "20 monitor_h-120",
     float = true,
 })
+
+-- "Smart gaps" / "No gaps when only"
+-- uncomment all if you wish to use that.
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({
+--     name        = "no-gaps-wtv1",
+--     match       = { float = false, workspace = "w[tv1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+-- hl.window_rule({
+--     name        = "no-gaps-f1",
+--     match       = { float = false, workspace = "f[1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
