f6a4b63885cb447111576b30b9daf51ca44f12ca
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-01 23:46:29 -0400

    better scratchpad for hyprland

--- hypr/.config/hypr/utils.lua
+++ hypr/.config/hypr/utils.lua
@@ -5,6 +5,10 @@ function M.webapp_class(url)
     return "brave-" .. url .. "__-Default"
 end
 
+function M.webapp_cmd(url)
+    return "brave --app=https://" .. url
+end
+
 ---@param app table
 ---@return function
 function M.spawn_or_focus(app)
@@ -22,9 +26,34 @@ end
 ---@return function
 function M.spawn_or_focus_webapp(url)
     return M.spawn_or_focus({
-        cmd = "brave --app=https://" .. url,
+        cmd = M.webapp_cmd(url),
         class = M.webapp_class(url),
     })
+end
+
+---@param scratchpad_name string
+---@param class string
+---@param cmd string|nil
+---@return function
+function M.scratchpad(scratchpad_name, class, cmd)
+    cmd = cmd or class
+    return function()
+        local w = hl.get_window("class:" .. class)
+        if w then
+            hl.dispatch(hl.dsp.workspace.toggle_special(scratchpad_name))
+        else
+            hl.dispatch(hl.dsp.exec_cmd(cmd, { workspace = scratchpad_name }))
+            hl.dispatch(hl.dsp.workspace.toggle_special(scratchpad_name))
+        end
+    end
+end
+
+---@param url string
+---@return function
+function M.spawn_or_focus_url(url)
+    return function()
+        hl.dispatch(hl.dsp.exec_cmd("brave --focus='https://" .. url .. "/*' https://" .. url))
+    end
 end
 
 function M.spawn_or_focus_tui(app)
