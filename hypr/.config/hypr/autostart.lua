f11b4afbd596f30b99290f81d742ab25896a24eb
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-04 18:51:08 -0400

    remove gtk qnd qt theming, only set dark mode

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,12 +1,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-local image = require("colors").image
-
 hl.on("hyprland.start", function()
     hl.exec_cmd("waybar -c ~/.config/waybar/hypr.jsonc")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("hyprsunset")
-    hl.exec_cmd("swaybg -i " .. image .. " -m fill")
-
     -- nixos asahi display issue workaround
     hl.exec_cmd(
         [[hyprctl dispatch 'hl.dsp.dpms({ action = "disable" })'; sleep 1; hyprctl dispatch 'hl.dsp.dpms({ action = "enable" })']]
@@ -20,4 +16,4 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
 
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
