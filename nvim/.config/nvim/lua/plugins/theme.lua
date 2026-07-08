0f852e6ef6608432db9090debc40286b2a55b108
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-07 22:17:27 -0400

    waybar style
    transparent nvim theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -2,11 +2,17 @@ vim.pack.add({
     { src = "https://github.com/catppuccin/nvim", version = vim.version.range("*") },
 })
 
+require("catppuccin").setup({
+    transparent_background = true,
+})
 vim.cmd("colorscheme catppuccin-mocha")
 
-vim.uv.new_signal():start("sigusr1", vim.schedule_wrap(function()
-    package.loaded["plugins.theme"] = nil
-    package.loaded["fzf-lua"] = nil
-    package.loaded["catppuccin"] = nil
-    require("plugins.theme")
-end))
+vim.uv.new_signal():start(
+    "sigusr1",
+    vim.schedule_wrap(function()
+        package.loaded["plugins.theme"] = nil
+        package.loaded["fzf-lua"] = nil
+        package.loaded["catppuccin"] = nil
+        require("plugins.theme")
+    end)
+)
