75d048f781983b19d1088c1260e5b556d316ae1a
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-23 15:50:40 -0400

    flatten dir struture targetting .config in stow
    remove xdg desktop items, managed by my nix config instead.
    remove explicit fish in config.

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
+end
+
+set -gx EDITOR "nvim"
+set -gx TERMINAL "kitty -1"
+set -gx PAGER "nvim +Man!"
+set -gx BROWSER "brave"
+
+fish_add_path -g $HOME/.config/scripts
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,43 @@
+vim.opt.showmode = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true -- ask for save on :q
+vim.opt.breakindent = true -- continue indentation
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+-- UI
+vim.opt.winborder = "rounded"
+vim.opt.pumborder = "rounded"
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.signcolumn = "yes"
+vim.opt.fillchars = "eob: "
+-- vim.opt.cmdheight = 0 -- hides cmd until used
+vim.opt.pumheight = 10 -- popup menu items
+-- Search
+vim.opt.ignorecase = true -- Ignore case in search patterns.
+vim.opt.smartcase = true -- Override the 'ignorecase' option if the search pattern contains upper case characters.
+-- completion
+vim.opt.completeopt = "menuone,noinsert,noselect,popup"
+-- diagnostics
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+    },
+})
+-- backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.undofile = true
