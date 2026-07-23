0133ac7ce8eb8b03a74dec058fae0a3ab0202303
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-23 16:21:14 -0400

    added npmrc env to read from .config

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,6 @@ set -gx EDITOR "nvim"
 set -gx TERMINAL "kitty -1"
 set -gx PAGER "nvim +Man!"
 set -gx BROWSER "brave"
+set -gx npm_config_userconfig "$HOME/.config/npm/.npmrc"
 
 fish_add_path -g $HOME/.config/scripts
