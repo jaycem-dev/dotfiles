80f64c68ed4fa08961f23fc626d0f7d144a7ced2
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-24 15:42:31 -0400

    move msg target in nvim ui2 msg

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -14,7 +14,7 @@ vim.pack.add({
 vim.cmd("packadd nvim.undotree")
 vim.keymap.set("n", "<leader>u", require("undotree").open)
 -- ui2 (experimental)
-require("vim._core.ui2").enable({ enable = true })
+require("vim._core.ui2").enable({ enable = true, msg = { target = "msg" } })
 
 -- simgle config / no config
 require("supermaven-nvim").setup({})
