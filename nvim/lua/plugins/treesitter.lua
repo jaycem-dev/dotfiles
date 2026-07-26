fdb751d44e6b1f76237d12278585625c06edc3f8
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-26 10:03:54 -0400

    fix nvim-treesitter install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,22 +3,43 @@ vim.pack.add({
 })
 
 local ts_parsers = {
-    "javascript",
-    "typescript",
-    "tsx",
     "astro",
-    "lua",
-    "html",
-    "css",
     "bash",
+    "css",
+    "diff",
+    "fish",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "go",
+    "html",
+    "htmldjango",
+    "hyprlang",
+    "ini",
+    "javascript",
+    "json",
+    "jsx",
+    "kdl",
+    "kitty",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "nix",
+    "nu",
     "python",
     "rust",
-    "go",
-    "kdl",
+    "sql",
+    "toml",
+    "tsx",
+    "typescript",
+    "yaml",
 }
 
 require("nvim-treesitter").setup({
-    -- fix for nixos, the dir needs to be set so it doesn't use the /store which has permissions issues.
+    -- fix for nixos, otherwise it uses /store which is read only
     install_dir = vim.fn.stdpath("data") .. "/site",
-    install = ts_parsers,
 })
+
+require("nvim-treesitter").install(ts_parsers)
