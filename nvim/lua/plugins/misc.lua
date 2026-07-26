0998e166f9b632221ef43884af3ae9c1394f2baa
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-26 12:21:12 -0400

    migrate to mini plugins to reduce deps

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,13 +1,13 @@
 -- small plugins with small or no config
 vim.pack.add({
     "https://github.com/catgoose/nvim-colorizer.lua",
-    "https://github.com/nvim-tree/nvim-web-devicons",
-    { src = "https://github.com/supermaven-inc/supermaven-nvim", version = "main" },
-    { src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("*") },
-    { src = "https://github.com/windwp/nvim-autopairs", version = vim.version.range("*") },
+    { src = "https://github.com/supermaven-inc/supermaven-nvim", version = vim.version.range("*") },
     { src = "https://github.com/folke/which-key.nvim", version = vim.version.range("*") },
-    { src = "https://github.com/lewis6991/gitsigns.nvim", version = vim.version.range("*") },
     { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim", version = vim.version.range("*") },
+    { src = "https://github.com/nvim-mini/mini.pairs", version = "stable" },
+    { src = "https://github.com/nvim-mini/mini.surround", version = vim.version.range("*") },
+    { src = "https://github.com/nvim-mini/mini.icons", version = vim.version.range("*") },
+    { src = "https://github.com/nvim-mini/mini.diff", version = vim.version.range("*") },
 })
 
 -- builtin plugins
@@ -18,9 +18,11 @@ require("vim._core.ui2").enable({ enable = true, msg = { target = "msg" } })
 
 -- simgle config / no config
 require("supermaven-nvim").setup({})
-require("nvim-autopairs").setup({})
 require("colorizer").setup()
-require("nvim-web-devicons").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
+require("mini.diff").setup()
+require("mini.icons").setup()
 require("which-key").setup({
     preset = "helix",
     delay = 500,
