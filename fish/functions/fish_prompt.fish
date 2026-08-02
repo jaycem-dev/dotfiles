fb6e91429fc2fff522345ef5bb38c1dbf141786a
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-08-02 07:14:44 -0400

    fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,34 +1,39 @@
-function fish_prompt --description 'Default prompt with added transient opt'
+function fish_prompt --description "Transient prompt"
     set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color --reset)
 
-    # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
+    set -l suffix "> "
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
         end
-        set suffix '#'
+        set suffix "# "
     end
 
+    set -l prompt_cwd (set_color $color_cwd)(prompt_pwd)$normal
+
     if contains -- --final-rendering $argv
-        echo -n -s (prompt_pwd | string split /)[-1] $suffix " "
+        echo -n -s $prompt_cwd $suffix
         return
     end
 
-    # Write pipestatus
-    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-    set -l bold_flag --bold
-    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-    if test $__fish_prompt_status_generation = $status_generation
-        set bold_flag
+    set -l suffix_color $normal
+    if test $last_pipestatus[-1] != 0
+        set suffix_color (set_color $fish_color_status)
     end
-    set __fish_prompt_status_generation $status_generation
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color $bold_flag $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY; and set -q fish_color_host_remote
+        set color_host $fish_color_host_remote
+    end
+
+    set -l prompt_host (set_color $color_host)"@"(prompt_hostname)$normal
+    set -l prompt_git (fish_vcs_prompt)
+    set -l prompt_nix ""
+    if set -q IN_NIX_SHELL
+        set prompt_nix (set_color $fish_color_option)" [nix-shell]"$normal
+    end
+
+    echo -n -s $prompt_host " " $prompt_cwd $prompt_git $prompt_nix $suffix_color $suffix
 end
