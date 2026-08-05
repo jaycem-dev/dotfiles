e8b3c9f6e1a3e25e0abc5fd8c084653e07d68f9b
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-08-05 18:49:20 -0400

    remove nvim signal to reload theme

--- themes/rose-pine-moon/nvim.lua
+++ themes/rose-pine-moon/nvim.lua
@@ -3,13 +3,3 @@ vim.pack.add({
 })
 
 vim.cmd("colorscheme rose-pine-moon")
-
-vim.uv.new_signal():start(
-    "sigusr1",
-    vim.schedule_wrap(function()
-        package.loaded["plugins.theme"] = nil
-        package.loaded["fzf-lua"] = nil
-        package.loaded["rose-pine"] = nil
-        require("plugins.theme")
-    end)
-)
--- themes/rose-pine/nvim.lua
+++ themes/rose-pine/nvim.lua
@@ -3,10 +3,3 @@ vim.pack.add({
 })
 
 vim.cmd("colorscheme rose-pine")
-
-vim.uv.new_signal():start("sigusr1", vim.schedule_wrap(function()
-    package.loaded["plugins.theme"] = nil
-    package.loaded["fzf-lua"] = nil
-    package.loaded["rose-pine"] = nil
-    require("plugins.theme")
-end))
