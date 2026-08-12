6c29ecfef55c706a36d1ddbd83b5423edeff0879
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-08-12 18:18:10 -0400

    new premade themes

---
+++ tinted-theming/themes/rose-pine-moon/nvim.lua
@@ -0,0 +1,24 @@
+vim.pack.add({
+  { src = "https://github.com/nvim-mini/mini.base16", version = "stable" },
+})
+
+require("mini.base16").setup({
+  palette = {
+    base00 = "#232136",
+    base01 = "#2a273f",
+    base02 = "#393552",
+    base03 = "#6e6a86",
+    base04 = "#908caa",
+    base05 = "#e0def4",
+    base06 = "#e0def4",
+    base07 = "#56526e",
+    base08 = "#eb6f92",
+    base09 = "#f6c177",
+    base0A = "#ea9a97",
+    base0B = "#3e8fb0",
+    base0C = "#9ccfd8",
+    base0D = "#c4a7e7",
+    base0E = "#f6c177",
+    base0F = "#56526e",
+  },
+})
