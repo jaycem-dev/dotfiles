e21867f6de683f5f19dd40d392dc99c6442bb512
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-08-19 22:27:57 -0400

    nvim keymap desc

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -20,8 +20,8 @@ vim.keymap.set("n", "<leader>gb", "<cmd>Pick git_branches<cr>", { desc = "Git br
 vim.keymap.set("n", "<leader>gc", "<cmd>Pick git_commits<cr>", { desc = "Git commits" })
 
 -- files & buffers
-vim.keymap.set("n", "<leader>fb", "<cmd>Pick buffers<cr>", { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Pick buffers<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Files" })
 
 -- search
 vim.keymap.set("n", "<leader>fg", "<cmd>Pick grep<cr>", { desc = "Grep" })
@@ -30,8 +30,6 @@ vim.keymap.set("n", "<leader>/", "<cmd>Pick grep_live<cr>", { desc = "Live grep"
 -- misc
 vim.keymap.set("n", "<leader><leader>", "<cmd>Pick resume<cr>", { desc = "Resume" })
 vim.keymap.set("n", "<leader>fo", "<cmd>Pick oldfiles<cr>", { desc = "Old files" })
-vim.keymap.set("n", "<leader>fk", "<cmd>Pick keymaps<cr>", { desc = "Find keymaps" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Pick help<cr>", { desc = "Find help" })
 
 -- LSP
 -- TODO: set
@@ -41,31 +39,33 @@ vim.keymap.set("n", "<leader>fh", "<cmd>Pick help<cr>", { desc = "Find help" })
 vim.keymap.set("n", "<leader>fa", vim.lsp.buf.code_action, { desc = "Code actions" })
 vim.keymap.set("n", "<leader>fr", function()
     pickers.lsp({ scope = "references" })
-end, { desc = "Find references (LSP)" })
+end, { desc = "LSP references" })
 vim.keymap.set("n", "<leader>fi", function()
     pickers.lsp({ scope = "implementation" })
-end, { desc = "Find implementations (LSP)" })
+end, { desc = "LSP implementations" })
 vim.keymap.set("n", "<leader>ft", function()
     pickers.lsp({ scope = "type_definition" })
-end, { desc = "Type definition" })
+end, { desc = "LSP Type definition" })
 vim.keymap.set("n", "<leader>fD", function()
     pickers.diagnostic({ scope = "all" })
-end, { desc = "Find diagnostics on workspace (LSP)" })
+end, { desc = "LSP workspace diagnostics" })
 vim.keymap.set("n", "<leader>fd", function()
     pickers.diagnostic({ scope = "current" })
-end, { desc = "Find diagnostics on current buffer (LSP)" })
+end, { desc = "LSP diagnostics" })
 vim.keymap.set("n", "<leader>fs", function()
     pickers.lsp({ scope = "document_symbol" })
-end, { desc = "Find document symbols (LSP)" })
+end, { desc = "LSP symbols" })
 vim.keymap.set("n", "<leader>fS", function()
     pickers.lsp({ scope = "workspace_symbol" })
-end, { desc = "Find workspace symbols (LSP)" })
+end, { desc = "LSP workspace symbols" })
 
 -- neovim
-vim.keymap.set("n", "<leader>fm", "<cmd>Pick marks<cr>", { desc = "Find marks" })
-vim.keymap.set("n", "<leader>vS", "<cmd>Pick spellsuggest<cr>", { desc = "Spell suggestions" })
+vim.keymap.set("n", "<leader>fm", "<cmd>Pick marks<cr>", { desc = "Marks" })
+vim.keymap.set("n", "<leader>vs", "<cmd>Pick spellsuggest<cr>", { desc = "Spell suggestions" })
+vim.keymap.set("n", "<leader>vk", "<cmd>Pick keymaps<cr>", { desc = "Keymaps" })
+vim.keymap.set("n", "<leader>vh", "<cmd>Pick help<cr>", { desc = "Help" })
 
 -- custom
 vim.keymap.set("n", "<leader>fc", function()
     require("mini.pick").builtin.files({}, { source = { cwd = vim.fn.expand("~/.config/nvim") } })
-end, { desc = "Find nvim config" })
+end, { desc = "Neovim config" })
